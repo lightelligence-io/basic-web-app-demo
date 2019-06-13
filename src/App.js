40bd239ef53c9dce5a3b1ec756703c947d3df7b8
Jonathan Stoye <mail@jonathanstoye.de>
2019-06-13 17:04:15 +0200

    feat: cleanup and basic setup
    Signed-off-by: Jonathan Stoye <mail@jonathanstoye.de>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Headline, Button } from "@lightelligence/react";
+import { Device } from "@lightelligence/browser-sdk";
+import styled from "styled-components/macro";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const StyledHeadline = styled(Headline)`
+  color: rebeccapurple;
+  font-family: sans-serif;
+`;
+
+export const App = ({ browserSDK }) => {
+  const [devices, setDevices] = useState([]);
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      const { data } = await Device.getDevices();
+      setDevices(data);
+    };
+    fetchDevices();
+  }, []);
+
+  const changeTenant = () => browserSDK.changeTenant();
+
+  const logout = () => browserSDK.logout();
+
+  const deviceEntrys = devices.map(({ id, info: { name } }) => (
+    <li key={id}>{name}</li>
+  ));
+
+  return (
+    <div>
+      <StyledHeadline size={1}>Devices</StyledHeadline>
+      <Button color="info" outline onClick={changeTenant}>
+        Change Tenant
+      </Button>
+      <Button color="error" outline onClick={logout}>
+        Log out
+      </Button>
+      <ul>{deviceEntrys}</ul>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@lightelligence/styles/dist/index.css";
+import "./index.css";
+import App from "./App";
+import BrowserSDK from "@lightelligence/browser-sdk/dist/bundle.web";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const browserSDK = new BrowserSDK({
+  environment: "prod",
+  clientId: "olt-portal"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+browserSDK.getCurrentUser().then(user => {
+  if (user) {
+    ReactDOM.render(
+      <App browserSDK={browserSDK} />,
+      document.getElementById("root")
+    );
+  } else {
+    browserSDK.login();
+  }
+});
